b0ea98e33b65a3a6619d72385fda89a897bfb4f1
Eldos Kubatov <eldos.220505@email.com>
2026-04-29 17:06:38 +0600

    first commit

---
+++ src/main/java/comsep23/digitaloceandevops/DigitaloceanDevopsApplication.java
@@ -0,0 +1,13 @@
+package comsep23.digitaloceandevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaloceanDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaloceanDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comsep23/digitaloceandevops/DigitaloceanDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package comsep23.digitaloceandevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaloceanDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
